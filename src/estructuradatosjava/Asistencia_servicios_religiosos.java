24be3f2c607a1e5b6fff779f9e612619a2e1653a
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2026-04-20 20:54:55 -0500

    Se realizan los loops por semana

--- src/estructuradatosjava/Asistencia_servicios_religiosos.java
+++ src/estructuradatosjava/Asistencia_servicios_religiosos.java
@@ -17,8 +17,15 @@ public class Asistencia_servicios_religiosos {
 		
         int [] asistentesMisaDiasMes = new int [30];
     	int sumaSemana = 0;
+    	
     	int inicioSemana1 = 0;
-    	int finSemana1 = 8;
+    	int finSemana1 = 7;
+    	
+    	int sumasemana1 = 0;
+    	double promedioSemana1 = 0;
+    	
+    	int inicioSemana2 = 8;
+    	int finSemana2 = 14;
 		
     	for (int i =0; i< asistentesMisaDiasMes.length; i++) {
     		System.out.println("Escriba el numero de asistentes a la mis el día " + " " + (i+1));
@@ -26,8 +33,14 @@ public class Asistencia_servicios_religiosos {
     	}
     	
 		for(int i = inicioSemana1 ; i<finSemana1; i++) {
-		sumaSemana += asistentesMisaDiasMes[i];
+		System.out.println(asistentesMisaDiasMes[i]);
+		sumasemana1 += asistentesMisaDiasMes[i];
 		}
+		promedioSemana1 = (double) sumasemana1 / 7;
+		System.out.println("Suma de la semana 1: " + sumasemana1);
+		System.out.println("promedio de la semana 1: " + promedioSemana1);
+		
+		
 		//for(int i = 0; i< asistentesMisaDiasMes[7]; i++ ) {
 			//sumaSemana += asistentesMisaDiasMes[i];
 			//System.out.println(asistentesMisaDiasMes[i]);
