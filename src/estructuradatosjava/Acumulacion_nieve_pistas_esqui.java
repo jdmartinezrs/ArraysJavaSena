b6ea8a06df2b385cb94e110195b57a2e7df7594e
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com>
2026-04-25 18:34:51 +0000

    Se obtiene el valor maximo del array aunque sin el value

--- src/estructuradatosjava/Acumulacion_nieve_pistas_esqui.java
+++ src/estructuradatosjava/Acumulacion_nieve_pistas_esqui.java
@@ -1,6 +1,8 @@
 package estructuradatosjava;
+import java.util.Arrays;
+import java.util.OptionalInt;
 import java.util.Scanner;
-
+import java.util.stream.IntStream;
 public class Acumulacion_nieve_pistas_esqui {
 
 public static void main(String[] args){
@@ -18,28 +20,26 @@ Scanner scanner = new Scanner(System.in);
 double Acumulacion= 0;
 int puntos = 10;
 
-double [] centimetroNieveEnPuntosMontana = new  double  [10];
-double maxAcumulacion = centimetroNieveEnPuntosMontana[0];
+
+int [] centimetroNieveEnPuntosMontana = new  int  [10];
+
 
 for (int i = 0 ; i < centimetroNieveEnPuntosMontana.length ; i++){
     System.out.println("Ingrese el nivel del punto:"+ (i+ 1));
-    centimetroNieveEnPuntosMontana[i] = scanner.nextDouble();
+    centimetroNieveEnPuntosMontana[i] = scanner.nextInt();
     Acumulacion  += centimetroNieveEnPuntosMontana[i];
    if(centimetroNieveEnPuntosMontana[i]< 30){
     System.out.println("El punto "+(i+1)+ " " + " tienen nieve insuficiente");
    }
   
 }
-System.out.println("La ACUMULACIÓN ES: "+Acumulacion);
- for(int nivel = 0; nivel < centimetroNieveEnPuntosMontana.length; nivel ++){
-    if (nivel>  maxAcumulacion){
-         maxAcumulacion = nivel;
-    }
-    
-   }
+IntStream intStream = Arrays.stream(centimetroNieveEnPuntosMontana);
+ OptionalInt optionalInt = intStream.max();
+    int maxAsInt = optionalInt.getAsInt();
+System.out.println("Maximum number = " + maxAsInt);
    double promedioAcumulacion = (double) Acumulacion / puntos;
  System.out.println("El promedio de acumulación es: "+ promedioAcumulacion );
-  System.out.println(" la zona de descenso más segura del día, es el punto: "+ maxAcumulacion);
+  System.out.println(" la zona de descenso más segura del día, es el punto: "+ maxAsInt);
 }
 
 }
